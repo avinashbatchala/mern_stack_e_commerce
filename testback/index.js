315a533ee8c2158b7a04b7f0b1aeacefbb26b0c2
Avinash Batchala <a.batchala@vikes.csuohio.edu>
2021-09-25 16:27:58 -0400

    express serve is instaleed and routes are created.

--- testback/index.js
+++ testback/index.js
@@ -1,3 +1,23 @@
-const express  = require("express");
-
+const express = require("express");
 const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send("Hi Avinash");
+});
+
+app.get('/login', (req, res) => {
+    return res.send("You are in login page")
+});
+
+app.get("/signout", (req, res) => {
+    return res.send("You are at signout page");
+});
+
+app.get("/instagram", (req, res) => {
+    return res.send("Avinash is using instagram")
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
